4e40baa70abb336bbcdd8abedcf76b2b5739612b
DeltaDemand <2390647320@qq.com>
2022-02-16 13:53:31 +0800

    完善修改配置功能

--- internal/inputArgs/parse_args.go
+++ internal/inputArgs/parse_args.go
@@ -9,8 +9,8 @@ import (
 
 // Parse :configServer命令行参数解析
 func Parse(confs *appConfigs.Config) {
-	flag.StringVar(&confs.ConfigServer.ServerName, "name", confs.ConfigServer.ServerName, "本Agent需连接的etcd上的修改配置的服务")
-	flag.StringVar(&confs.ConfigServer.ListenPort, "p", confs.ConfigServer.ListenPort, "etcd上Agent分组")
+	flag.StringVar(&confs.ConfigServer.ServerName, "name", confs.ConfigServer.ServerName, "本服务的名称")
+	flag.StringVar(&confs.ConfigServer.ListenPort, "p", confs.ConfigServer.ListenPort, "开放的端口")
 
 	var ends string
 	flag.StringVar(&ends, "endPoints", "112.74.60.132:2379", "etcd节点的地址可以多个，用逗号(,)隔开")
